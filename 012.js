ba6efa5dbf5c3f5f6972f39e8a34eda13553724d
Rogerskelamen <rogerskelamen@gmail.com>
2022-04-26 16:58:06 +0800

    Q12

--- 012.js
+++ 012.js
@@ -5,5 +5,15 @@
  * @return {string}
  */
 var intToRoman = function(num) {
-
+  let one   = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
+  let two   = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
+  let three = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
+  let four  = ['', 'M', 'MM', 'MMM']
+  return four[parseInt(num / 1000)] + three[parseInt(num % 1000 / 100)] + two[parseInt(num % 100 / 10)] + one[parseInt(num % 10)]
 };
+
+/**
+ * Test below
+ * 下面是测试代码
+ */
+console.log(intToRoman(1994));
